517e3398861815d50f8d409ab5eb0561fe6a3973
Otto Martin <otto.martin@pinja.com>
2021-02-25 13:42:41 +0200

    Lab 05

--- index.js
+++ index.js
@@ -5,25 +5,37 @@ const port = 3000
 
 app.use(express.static('public'))
 
-// app.get('/api/locations', (req, res) => {  res.send('fetch all locations')  })
-
 let database = 
 [
     { id: 1, latitude: 60, longitude: 70 },
     { id: 2, latitude: 40, longitude: 80 },
 ];
 
-app.get('/api/locations', (req, res) => {  res.json(database);  })
-
+var locationsrouter = express.Router()
+locationsrouter.get('/', (req, res) => {  res.json(database);  })
+locationsrouter.get('/1', (req, res) => {  res.json(database[0]);  })
+/*
+// Pretty print: create string 
 app.get('/api/locations/pretty_json', (req, res) => 
 {  
     res.set('Content-Type', 'application/json')
     res.send(JSON.stringify(database, null, 4));  
 })
-
-
-// Pretty print: create string 
+*/
+app.use('/api/locations', locationsrouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// app.get('/api/locations', (req, res) => {  res.send('fetch all locations')  })
+/*
+app.use('/hello', (req, res, next) => {
+    console.log('hello')
+    next()
+  })
+  app.use('/world', (req, res, next) => {
+    console.log('world')
+    next()
+  })
+*/
