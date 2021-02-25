408e942640ca2a6142c3463b1889993a122076aa
Otto Martin <otto.martin@pinja.com>
2021-02-25 14:29:16 +0200

    Lab 07

--- routes/locations.js
+++ routes/locations.js
@@ -4,6 +4,7 @@ let database =
 [
     { id: 1, latitude: 60, longitude: 70 },
     { id: 2, latitude: 40, longitude: 80 },
+    { id: 3, latitude: 60, longitude: 60 },
 ];
 
 var locationsrouter = express.Router()
