f16f30cb490b89bd92256d099f92f967324faf09
Otto Martin <otto.martin@pinja.com>
2021-02-25 13:53:18 +0200

    Lab 06

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-// empty
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+let locations_router = require('./routes/locations.js');
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-let database = 
-[
-    { id: 1, latitude: 60, longitude: 70 },
-    { id: 2, latitude: 40, longitude: 80 },
-];
-
-var locationsrouter = express.Router()
-locationsrouter.get('/', (req, res) => {  res.json(database);  })
-locationsrouter.get('/1', (req, res) => {  res.json(database[0]);  })
-/*
-// Pretty print: create string 
-app.get('/api/locations/pretty_json', (req, res) => 
-{  
-    res.set('Content-Type', 'application/json')
-    res.send(JSON.stringify(database, null, 4));  
-})
-*/
-app.use('/api/locations', locationsrouter)
+app.use('/api/locations', locations_router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
-
-// app.get('/api/locations', (req, res) => {  res.send('fetch all locations')  })
-/*
-app.use('/hello', (req, res, next) => {
-    console.log('hello')
-    next()
-  })
-  app.use('/world', (req, res, next) => {
-    console.log('world')
-    next()
-  })
-*/
+});
---
+++ routes/locations.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+let database = 
+[
+    { id: 1, latitude: 60, longitude: 70 },
+    { id: 2, latitude: 40, longitude: 80 },
+];
+
+var locationsrouter = express.Router()
+locationsrouter.get('/', (req, res) => {  res.json(database);  })
+locationsrouter.get('/1', (req, res) => {  res.json(database[0]);  })
+
+module.exports = locationsrouter;
+
+/*
+// Pretty print: create string 
+app.get('/api/locations/pretty_json', (req, res) => 
+{  
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(database, null, 4));  
+})
+*/
