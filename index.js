f959925fe8b35134580c6aba08f596a8925cfa81
Otto Martin <otto.martin@pinja.com>
2021-02-25 11:40:05 +0200

    Lab 04 + extra

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const port = 3000
 
 app.use(express.static('public'))
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/api/locations', (req, res) => {  res.send('fetch all locations')  })
+
+let database = 
+[
+    { id: 1, latitude: 60, longitude: 70 },
+    { id: 2, latitude: 40, longitude: 80 },
+];
+
+app.get('/api/locations', (req, res) => {  res.json(database);  })
+
+app.get('/api/locations/pretty_json', (req, res) => 
+{  
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.stringify(database, null, 4));  
 })
-*/
+
+
+// Pretty print: create string 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
